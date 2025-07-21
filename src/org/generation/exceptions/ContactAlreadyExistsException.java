1f70b08c68c207ca7233e10167a7940fdac1809b
Alejandro <asotomiguel11@gmail.com>
2025-07-21 12:38:29 -0600

    Correcion Exceptions

--- src/org/generation/exceptions/ContactAlreadyExistsException.java
+++ src/org/generation/exceptions/ContactAlreadyExistsException.java
@@ -3,8 +3,7 @@ package org.generation.exceptions;
 // Excepción lanzada cuando se intenta crear un contacto que ya existe en el sistema.
 public class ContactAlreadyExistsException extends RuntimeException {
 
-    public ContactAlreadyExistsException(String contactName) {
-        super("El contacto '" + contactName + "' ya existe en la agenda. "
-                + "Intente con un nombre diferente.");
+    public ContactAlreadyExistsException(String message) {
+        super(message);
     }
 }
