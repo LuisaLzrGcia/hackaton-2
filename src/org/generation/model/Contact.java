743150bb276dfdabb5ea06dc2b8268dd55653902
Sebastian Capistran <teenissuebeats@gmail.com>
2025-07-21 11:47:52 -0600

    updateModel

--- src/org/generation/model/Contact.java
+++ src/org/generation/model/Contact.java
@@ -1,4 +1,19 @@
 package org.generation.model;
 
 public class Contact {
+    private String name;
+    private String phone;
+
+    public Contact(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
