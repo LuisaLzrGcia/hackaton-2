caeb427dc8cf2016fa0e7287ac617f6f26fc4393
LuisaLzrGcia <maluisa.lzrgcia@gmail.com>
2025-07-21 12:52:20 -0600

    validacion al añadir contacto

--- src/org/generation/service/ContactServices.java
+++ src/org/generation/service/ContactServices.java
@@ -10,9 +10,43 @@ import java.util.List;
 public class ContactServices {
     private final List<Contact> contacts = new ArrayList<>();
 
+    // Verificar si un contacto es valido
+    public void validateContact(Contact contact) {
+        if (contact == null) {
+            throw new InvalidContactException("El contacto no puede ser null.");
+        }
+
+        if (contact.getName() == null || contact.getName().isBlank()) {
+            throw new InvalidContactException("El nombre no puede estar vacío.");
+        }
+
+
+        if (contact.getPhone() == null || contact.getPhone().isBlank()) {
+            throw new InvalidContactException("El teléfono no puede estar vacío.");
+        }
+
+        if (!contact.getPhone().matches("\\d{10}")) {
+            throw new InvalidContactException("El teléfono debe contener solo dígitos y tener entre 10 caracteres.");
+        }
+    }
+
+
     //--- Agregar contacto
     public void addContact(Contact contact) {
+
+        validateContact(contact);
+
+        if (contactExists(contact)) {
+            throw new IllegalArgumentException("El contacto ya existe en la agenda.");
+        }
+
+        if (isFull()) {
+            throw new IllegalArgumentException("El contacto ya existe en la agenda.");
+        }
+
         contacts.add(contact);
+
+
     }
 
     //--- Listar contactos
@@ -25,7 +59,7 @@ public class ContactServices {
         Iterator<Contact> iterator = contacts.iterator();
         while (iterator.hasNext()) {
             Contact contact = iterator.next();
-            if (contact.getNameContact().equals(name)) {
+            if (contact.getName().equals(name)) {
                 return contact;
             }
         }
@@ -37,11 +71,23 @@ public class ContactServices {
         Iterator<Contact> iterator = contacts.iterator();
         while (iterator.hasNext()) {
             Contact contact = iterator.next();
-            if (contact.getPhoneContact().equals(phone)) {
+            if (contact.getPhone().equals(phone)) {
                 return contact;
             }
         }
         throw new ContactNotFoundException("No se encuentra el contacto con telefono: " + phone);
+    }
+
+    //--- El contacto existe
+    public boolean contactExists(Contact currentContact) {
+        Iterator<Contact> iterator = contacts.iterator();
+        while (iterator.hasNext()) {
+            Contact contact = iterator.next();
+            if (contact.getName().equals(currentContact.getName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
