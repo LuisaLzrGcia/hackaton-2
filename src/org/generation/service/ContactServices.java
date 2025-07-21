d44144ba6472b4ad5ff9a71c0cf7d4ca45a36d19
LuisaLzrGcia <maluisa.lzrgcia@gmail.com>
2025-07-21 11:12:18 -0600

    addContact

--- src/org/generation/service/ContactServices.java
+++ src/org/generation/service/ContactServices.java
@@ -1,4 +1,18 @@
 package org.generation.service;
 
+import org.generation.model.Contact;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class ContactServices {
+    private final List<Contact> contacts = new ArrayList<>();
+
+    //--- Agregar contacto
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+
 }
