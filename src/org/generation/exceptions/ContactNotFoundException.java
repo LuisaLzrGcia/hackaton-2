5fa5c2447c984655f0f70804294ef25c25c9a54c
Alejandro <asotomiguel11@gmail.com>
2025-07-21 12:59:50 -0600

    Exception

--- src/org/generation/exceptions/ContactNotFoundException.java
+++ src/org/generation/exceptions/ContactNotFoundException.java
@@ -3,8 +3,7 @@ package org.generation.exceptions;
 // Excepción lanzada cuando no se encuentra un contacto con el nombre y apellido especificado.
 public class ContactNotFoundException extends RuntimeException {
 
-    public ContactNotFoundException(String name) {
-        super("No se encontró ningún contacto con el nombre: '" + name + "'. "
-                + "Verifique que el nombre está escrito correctamente.");
+    public ContactNotFoundException(String message) {
+        super(message);
     }
 }
